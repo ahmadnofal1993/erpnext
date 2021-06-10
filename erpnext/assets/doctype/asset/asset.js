352811907626987f7a057706dbc3cd7f377bdb02
AhmadNofal <Ahmad.Nofal@albadiacement.com>
2021-06-10 08:03:55 +0000

    mychange

--- erpnext/assets/doctype/asset/asset.js
+++ erpnext/assets/doctype/asset/asset.js
@@ -5,6 +5,7 @@ frappe.provide("erpnext.asset");
 
 frappe.ui.form.on('Asset', {
 	onload: function(frm) {
+		console.log('hi asset00');
 		frm.set_query("item_code", function() {
 			return {
 				"filters": {
